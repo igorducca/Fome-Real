0c8ae2b19cfb80e2035e2703ddfd761a14889991
InstaLiveViewer <seandzn@hotmail.com>
2020-10-24 23:09:41 -0300

    Updates

---
+++ precache-manifest.723ee063988d96e98b0eb0faa775c2ce.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c46f25fa7834f3a00f458044dced684",
+    "url": "/Fome-Real/index.html"
+  },
+  {
+    "revision": "a1c818428e87012a6c43",
+    "url": "/Fome-Real/static/css/2.bcddf60d.chunk.css"
+  },
+  {
+    "revision": "b07041619d172da66427",
+    "url": "/Fome-Real/static/css/main.49d0bb3b.chunk.css"
+  },
+  {
+    "revision": "a1c818428e87012a6c43",
+    "url": "/Fome-Real/static/js/2.b1a762b6.chunk.js"
+  },
+  {
+    "revision": "3735b4f296546ca056ef36d11eb34563",
+    "url": "/Fome-Real/static/js/2.b1a762b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07041619d172da66427",
+    "url": "/Fome-Real/static/js/main.0dbb5710.chunk.js"
+  },
+  {
+    "revision": "e367ac908c52a1cebf7f",
+    "url": "/Fome-Real/static/js/runtime-main.211df68c.js"
+  },
+  {
+    "revision": "fbcce0de5b74ac504c7e45e75a0e5ddd",
+    "url": "/Fome-Real/static/media/duotone-wallpaepr.fbcce0de.png"
+  },
+  {
+    "revision": "4df4959e82c415e8b65f48576607f71e",
+    "url": "/Fome-Real/static/media/fome-real-png.4df4959e.png"
+  },
+  {
+    "revision": "b71b7d6046bdfbee99ed17e1c3adecdf",
+    "url": "/Fome-Real/static/media/loading.b71b7d60.gif"
+  },
+  {
+    "revision": "51557b6052fcdb8996af1dafa86d90b4",
+    "url": "/Fome-Real/static/media/pedido-background.51557b60.png"
+  },
+  {
+    "revision": "42329f0a25acc63970b63b91a50045ed",
+    "url": "/Fome-Real/static/media/sapiens-bike.42329f0a.svg"
+  },
+  {
+    "revision": "bf1763d98414a12e70234155a67d6cb4",
+    "url": "/Fome-Real/static/media/sucess-devices.bf1763d9.png"
+  }
+]);
